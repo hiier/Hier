eddb7841f153158eef934cf136d5c57a02adeca9
P.R.K <renky@PRKs-MacBook-Air.local>
2017-06-21 20:08:11 -0700

    First commit

---
+++ Hier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hier
+//
+//  Created by P.R.K on 6/20/17.
+//  Copyright © 2017 P.R.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var EmailEnter: UITextField!
+    
+    
+    @IBOutlet weak var passwordEnter: UITextField!
+    
+    
+    @IBOutlet weak var register: UIButton!
+    
+    
+    @IBOutlet weak var label: UILabel!
+
+    
+}
+
